dee321b4146d5d7d81b9d7b149f52fcff31bcb6c
SarathKumar <8607290+mcsarathkumar@users.noreply.github.com>
2023-08-01 22:24:55 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,3 +1,3 @@
 [default]
-aws_access_key_id=AKIAZBIDVC4DPLV3GVMT
-aws_secret_access_key=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
+aws_access_key_id = AKIAZBIDVC4DPLV3GVMT
+aws_secret_access_key = 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
